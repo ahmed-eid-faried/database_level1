b544afdf0c0c1ab09c025662b3cc361e903a1e22
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-28 18:13:30 +0300

    PRIMARY KEY

--- constraints.sql
+++ constraints.sql
@@ -7,3 +7,22 @@
 -- CHECK - Ensures that the values in a column satisfies a specific condition
 -- DEFAULT - Sets a default value for a column if no value is specified
 -- CREATE INDEX - Used to create and retrieve data from the database very quickly
+
+-- Constraint
+CREATE TABLE Persons(
+    ID int not null primary key,
+    FirstName NVARCHAR(255) not null,
+    LastName NVARCHAR(255) not null,
+    BirthDay DATE,
+);
+CREATE TABLE Persons2 (
+   ID int NOT NULL,
+   LastName NVARCHAR(255) NOT NULL,
+   FirstName NVARCHAR(255) NOT NULL,
+   Age int,
+   CONSTRAINT PK_Person PRIMARY KEY (ID,LastName)
+);
+ALTER TABLE Persons2 DROP CONSTRAINT PK_Person;
+ALTER TABLE Persons2 ADD PRIMARY KEY (ID);
+ALTER TABLE Persons2 ADD CONSTRAINT PK_Person PRIMARY KEY (ID);
+ALTER TABLE Persons2 ADD CONSTRAINT PK_Person PRIMARY KEY (ID,LastName);
