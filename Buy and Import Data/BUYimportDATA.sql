420662231eda31d4f5fd98b6f58d2f2a77de704b
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-10-06 17:26:46 +0300

    NORMALIZATION FOR REAL DATA

--- Buy and Import Data/BUYimportDATA.sql
+++ Buy and Import Data/BUYimportDATA.sql
@@ -1,3 +1,29 @@
+SELECT   *  FROM CarDetialsTable;
 SELECT TOP (1000) *  FROM CarDetialsTable;
 SELECT   Count(*)  FROM CarDetialsTable;
 SELECT   Count(*)  FROM CarDetialsTable WHERE Make='BMW';
+
+--create new copy from table
+SELECT * INTO CarDetialsTable2 FROM CarDetialsTable;
+ 
+SELECT DISTINCT Make FROM CarDetialsTable Order by Make Asc;
+
+CREATE TABLE Makes(
+    MakeID INT NOT NULL identity(1,1),
+    Make VARCHAR(50) UNIQUE NOT NULL,
+    PRIMARY KEY(MakeID),
+)
+
+INSERT INTO Makes
+SELECT DISTINCT Make FROM CarDetialsTable Order by Make Asc;
+
+SELECT * FROM Makes;
+
+ALTER TABLE CarDetialsTable2 ADD MakeID INT ;
+ 
+UPDATE CarDetialsTable2
+set MakeID =(SELECT MakeID FROM Makes WHERE (Makes.Make=CarDetialsTable2.Make));
+
+SELECT TOP (1000) *  FROM CarDetialsTable2;
+
+ALTER TABLE CarDetialsTable2  DROP Make;
