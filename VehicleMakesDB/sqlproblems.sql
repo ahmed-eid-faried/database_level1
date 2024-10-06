df3eda2ada477675d96a05567cf41aa7af56c8ce
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-10-06 22:40:25 +0300

    Problem 6: Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside

--- VehicleMakesDB/sqlproblems.sql
+++ VehicleMakesDB/sqlproblems.sql
@@ -77,3 +77,16 @@ Group by Make
 where R.NumberOfVehicles > 12000
 -----------------------------------------------------------------
 -----------------------------------------------------------------
+--  Problem 6: Get number of vehicles made between 1950 and 2000 per make and add total vehicles column 
+-- select   count(*) as NumberOfVehicles from VehicleDetails;
+
+select Makes.Make, count(*) as NumberOfVehicles,TotalVehicles=(select count(*) as NumberOfVehicles from VehicleDetails)
+--  ,((count(*) / TotalVehicles) * 100) as p
+from VehicleDetails
+inner join Makes on (Makes.MakeID = VehicleDetails.MakeID)
+where VehicleDetails.Year between 1950 and 2000
+Group by Make
+order by NumberOfVehicles Desc;
+
+-----------------------------------------------------------------
+-----------------------------------------------------------------
