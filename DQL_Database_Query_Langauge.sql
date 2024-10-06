00b1963f0931f6e6d46de777746cebc5716e9800
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-10-06 19:48:28 +0300

    sql problems

--- DQL_Database_Query_Langauge.sql
+++ DQL_Database_Query_Langauge.sql
@@ -47,15 +47,13 @@ where DepartmentID=1 Or DepartmentID=2 or DepartmentID=5 or DepartmentID=7;
 select * from Employees
 where DepartmentID in (1,2,5,7);
 
-
 select * from Employees
 where FirstName in ('Jacob','Brooks','Harper');
 
 
 select Departments.Name from Departments 
 where 
-ID in ( select DepartmentID from Employees where MonthlySalary <=210 );
-
+ID in ( select DepartmentID from Employees where MonthlySalary <=210 ); 
 
 select Departments.Name from Departments 
 where 
@@ -199,6 +197,7 @@ Select * from Employees where
 
 Select * from Employees where
 MonthlySalary Between 500 and 1000;
+
 ------------------------------------------------------
 ----------Count, Sum, Avg, Min, Max Functions---------
 ------------------------------------------------------
