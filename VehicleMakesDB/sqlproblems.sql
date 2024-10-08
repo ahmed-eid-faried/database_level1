739148c9414e0657bf3346872766b39cbe6e35de
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-10-08 05:46:49 +0300

    -- Problem 7: Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside it, then calculate it's percentage

--- VehicleMakesDB/sqlproblems.sql
+++ VehicleMakesDB/sqlproblems.sql
@@ -90,3 +90,15 @@ order by NumberOfVehicles Desc;
 
 -----------------------------------------------------------------
 -----------------------------------------------------------------
+use VehicleMakesDB;
+-- Problem 7: Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside it, then calculate it's percentage
+SELECT
+    Makes.Make,
+    COUNT(*) AS NumberOfVehicles,
+    ( SELECT COUNT(*) AS TotalVehicleCount FROM VehicleDetails) AS TotalVehicles,
+    (COUNT(*) * 100.0 / (SELECT COUNT(*) AS TotalVehicleCount FROM VehicleDetails )) AS Percentage
+from VehicleDetails
+    inner join Makes on (Makes.MakeID = VehicleDetails.MakeID)
+where VehicleDetails.Year between 1950 and 2000
+Group by Make
+order by NumberOfVehicles Desc;
