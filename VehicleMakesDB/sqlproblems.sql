0977b2c8e68891e0affb2b82ac333cf2dc352ac9
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-10-11 12:04:45 +0300

    --   Problem 34: Get all vehicles that have the minimum Engine_CC

--- VehicleMakesDB/sqlproblems.sql
+++ VehicleMakesDB/sqlproblems.sql
@@ -380,3 +380,9 @@ ORDER BY Age Desc;
 --   Problem 33: Get Minimum Engine CC , Maximum Engine CC , and Average Engine CC of all Vehicles
 SELECT MIN(VehicleDetails.Engine_CC) AS 'Minimum Engine CC', MAX(VehicleDetails.Engine_CC) AS 'Maximum Engine CC', AVG(VehicleDetails.Engine_CC) AS 'Average Engine CC'
 FROM VehicleDetails;
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+--   Problem 34: Get all vehicles that have the minimum Engine_CC
+SELECT * FROM VehicleDetails;
+-----------------------------------------------------------------
+-----------------------------------------------------------------
