ace325147017ff06b388c45e55411101f2255933
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-10-12 12:14:51 +0300

    --   Problem 52: Get all employees that have manager or does not have manager along with Manager's name, incase no manager name show null

--- EmployeesDB/Self Referential Queries PROBLEMS.sql
+++ EmployeesDB/Self Referential Queries PROBLEMS.sql
@@ -3,7 +3,7 @@ USE EmployeesDB;
 -----------------------------------------------------------------
 --   Problem 51: Get all employees that have manager along with Manager's name.
 SELECT * FROM Employees;
-SELECT Employees.EmployeeID, Employees.Name, Employees.Salary, Employees.ManagerID, Managers.Name FROM Employees 
+SELECT Employees.EmployeeID, Employees.Name, Employees.Salary, Employees.ManagerID, Managers.Name AS Manager FROM Employees 
 INNER JOIN Employees AS Managers ON Employees.ManagerID=Managers.EmployeeID
 WHERE Employees.ManagerID IS NOT NULL;
 
@@ -13,3 +13,7 @@ WHERE Employees.ManagerID IS NOT NULL;
 -- Note we used inner join on the same table with diffrent alliace.
 -----------------------------------------------------------------
 -----------------------------------------------------------------
+--   Problem 52: Get all employees that have manager or does not have manager along with Manager's name, incase no manager name show null
+SELECT Employees.EmployeeID, Employees.Name, Employees.Salary, Employees.ManagerID, Managers.Name AS Manager FROM Employees 
+LEFT JOIN Employees AS Managers ON Employees.ManagerID=Managers.EmployeeID;
+ 
