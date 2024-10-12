be1e7db1ffcc53183c32bddb7cd8c10dd47dcc3c
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-10-12 11:48:59 +0300

    --   Problem 50: Get all Fuel Types , each time the result should be showed in random order

--- VehicleMakesDB/sqlproblems.sql
+++ VehicleMakesDB/sqlproblems.sql
@@ -590,3 +590,5 @@ from
 ) R1);
 -----------------------------------------------------------------
 -----------------------------------------------------------------
+--   Problem 50: Get all Fuel Types , each time the result should be showed in random order
+SELECT * FROM FuelTypes ORDER BY NEWID();
