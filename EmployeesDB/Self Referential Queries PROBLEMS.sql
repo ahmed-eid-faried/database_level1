c01a98d3dcde6bc943362182dcde9918f22a860e
AHMED Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-10-12 12:11:05 +0300

    --   Problem 51: Get all employees that have manager along with Manager's name.

---
+++ EmployeesDB/Self Referential Queries PROBLEMS.sql
@@ -0,0 +1,15 @@
+USE EmployeesDB;
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+--   Problem 51: Get all employees that have manager along with Manager's name.
+SELECT * FROM Employees;
+SELECT Employees.EmployeeID, Employees.Name, Employees.Salary, Employees.ManagerID, Managers.Name FROM Employees 
+INNER JOIN Employees AS Managers ON Employees.ManagerID=Managers.EmployeeID
+WHERE Employees.ManagerID IS NOT NULL;
+
+-- Get all employees that have manager along with Manager's name.
+-- this will select all data from employees that are managed by someone along with their manager name, 
+-- employees that have no manager will not be selected because we used inner join 
+-- Note we used inner join on the same table with diffrent alliace.
+-----------------------------------------------------------------
+-----------------------------------------------------------------
--- VehicleMakesDB/sqlproblems.sql
+++ VehicleMakesDB/sqlproblems.sql
@@ -592,3 +592,5 @@ from
 -----------------------------------------------------------------
 --   Problem 50: Get all Fuel Types , each time the result should be showed in random order
 SELECT * FROM FuelTypes ORDER BY NEWID();
+-----------------------------------------------------------------
+-----------------------------------------------------------------
